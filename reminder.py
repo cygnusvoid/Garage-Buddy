95b74ee1cc9d988523587d9068c255c867a38df4
Will Hobbs <89028271+cygnusvoid@users.noreply.github.com>
2022-05-05 23:17:39 -0400

    refining code
    Code now works! (to an extent)
    
    added icon, changed notification sound and length

--- reminder.py
+++ reminder.py
@@ -3,13 +3,13 @@ from win10toast import ToastNotifier
 
 #building reminder within a toast notification
 def timer (reminder,seconds):
-    toaster = ToastNotifier
+    toaster = ToastNotifier()
 
-    toaster.show_toast (title="Reminder", msg="Alarm will go off in (seconds) seconds.", icon_path=None , duration = 20)
-    toaster.show_toast (f"Reminder", reminder, duration = 20)
+    toaster.show_toast(title = "Reminder", msg = "Alarm will go off in (seconds) seconds.", icon_path = "C:/Users/Hobbs/Downloads/GB-wrench.ico", duration = 10)
+    toaster.show_toast(title = "Reminder", msg = reminder, icon_path = "C:/Users/Hobbs/Downloads/GB-wrench.ico", duration = 5)
 
 #alarm sound, still within "timer" function
-    frequency = 2500
+    frequency = 500
     duration = 1000
     winsound.Beep(frequency,duration)
 
