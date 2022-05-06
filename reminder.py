be68f026eaf290cf74a19b7f671eab2577721f61
Will Hobbs <89028271+cygnusvoid@users.noreply.github.com>
2022-05-05 22:03:52 -0400

    Input
    added code for user input, as well as calling the "timer" function

--- reminder.py
+++ reminder.py
@@ -8,8 +8,14 @@ def timer (reminder,seconds):
     toaster.show_toast ("Reminder", f """Alarm will go off in (seconds) seconds.""", duration = 20)
     toaster.show_toast (f"Reminder", reminder, duration = 20)
 
-
 #alarm sound, still within "timer" function
     frequency = 2500
     duration = 1000
     winsound.Beep(frequency,duration)
+
+#user input and calling "timer" function
+if __name__ == "__main__":
+    words = input ("What would you like to be reminded of? ")
+    seconds = int(input("Enter seconds: "))
+
+    timer (words, seconds)
