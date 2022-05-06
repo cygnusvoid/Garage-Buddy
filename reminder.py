088a6bda6fc7b8e2668fa29e9124b24cac2acb16
Will Hobbs <89028271+cygnusvoid@users.noreply.github.com>
2022-05-06 00:39:41 -0400

    refining code
    new notification jingle added,
    minutes are now used instead of seconds for desired remind time

--- reminder.py
+++ reminder.py
@@ -5,17 +5,29 @@ from win10toast import ToastNotifier
 def timer (reminder,seconds):
     toaster = ToastNotifier()
 
-    toaster.show_toast(title = "Reminder", msg = "Alarm will go off in (seconds) seconds.", icon_path = "C:/Users/Hobbs/Downloads/GB-wrench.ico", duration = 10)
-    toaster.show_toast(title = "Reminder", msg = reminder, icon_path = "C:/Users/Hobbs/Downloads/GB-wrench.ico", duration = 5)
+    toaster.show_toast(title = "Reminder", msg = "Alarm will go off in "+ str(seconds) + " minute(s).", icon_path = "C:/Users/Hobbs/Downloads/GB-wrench.ico", duration = seconds*60)
+    toaster.show_toast(title = "Reminder", msg = reminder, icon_path = "C:/Users/Hobbs/Downloads/GB-wrench.ico", duration = 3)
 
 #alarm sound, still within "timer" function
-    frequency = 500
-    duration = 1000
+    frequency = 523
+    duration = 500
+    winsound.Beep(frequency,duration)
+    frequency = 784
+    duration = 200
+    winsound.Beep(frequency,duration)
+    frequency = 1047
+    duration = 200
+    winsound.Beep(frequency,duration)
+    frequency = 523
+    duration = 300
+    winsound.Beep(frequency,duration)
+    frequency = 523
+    duration = 300
     winsound.Beep(frequency,duration)
 
 #user input and calling "timer" function
 if __name__ == "__main__":
     words = input ("What would you like to be reminded of? ")
-    seconds = int(input("Enter seconds: "))
+    seconds = int(input("In how many minutes would you like to be reminded? "))
 
     timer (words, seconds)
