7cd9fc15eb127d35592daa2069a673cd56a3324b
Will Hobbs <89028271+cygnusvoid@users.noreply.github.com>
2022-05-05 21:46:08 -0400

    Alarm
    coded alarm sound, also made changes to project roadmap

--- reminder.py
+++ reminder.py
@@ -7,3 +7,9 @@ def timer (reminder,seconds):
 
     toaster.show_toast ("Reminder", f """Alarm will go off in (seconds) seconds.""", duration = 20)
     toaster.show_toast (f"Reminder", reminder, duration = 20)
+
+
+#alarm sound, still within "timer" function
+    frequency = 2500
+    duration = 1000
+    winsound.Beep(frequency,duration)
